9f62c883f26726a2c40bb703222870684cee5413
Andy-Lonly <own@renme.org>
2014-10-15 17:18:12 +0800

    搜索mod值添加

--- struct/global/header.php
+++ struct/global/header.php
@@ -4,6 +4,6 @@
 <div id="headerSubContainer">
 	<a href="?mode="><li><i class="fa fa-home"></i> 黑屋主页</li></a>
     <a href="?mode=editrecord"><li><i class="fa fa-plus-circle"></i> 添加/删除记录</li></a>
-    <a href="?mode=search"><li><i class="fa fa-search"></i> 搜索记录</li></a>
+    <a href="?mode=select"><li><i class="fa fa-search"></i> 搜索记录</li></a>
     <a href="?mode=about"><li><i class="fa fa-info"></i> 关于black-house</li></a>
 </div>
