c19dab7a5956f04b1ff5e5f072b537774f41592a
Andy-Lonly <own@renme.org>
2014-10-15 10:06:17 +0800

    调整部分表单addNewName.php

--- struct/addNewName.php
+++ struct/addNewName.php
@@ -1,7 +1,7 @@
 <form method="post" id="addNewRecForm" action="?mode=script&action=addNewRecord">
 	<input type="text" name="username" placeholder="用户名" /><br>
     <input type="text" name="qqnum" placeholder="QQ号" /><br>
-    <input type="text" name="level" placeholder="等级" /><br>
+    <input type="number" name="level"max="3" min="0" step="1" required="required" placeholder="等级" /><br>
     <textarea form="addNewRecForm" name="reason" placeholder="原因" /><br>
     <button type="submit">提交</button>
 </form>
