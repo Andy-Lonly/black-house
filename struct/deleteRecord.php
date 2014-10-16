0d5eb854c84b2287426818fe74c1f18ed5ab53d1
Andy-Lonly <own@renme.org>
2014-10-16 17:48:29 +0800

    Update deleteRecord.php

--- struct/deleteRecord.php
+++ struct/deleteRecord.php
@@ -1,4 +1,5 @@
 <form method="post" action="?mode=script&action=deleteRecord">
-    <input name="qqnum" type="text" placeholder="请输入信息" /><br>
+  <input type="text" name="qqnum" placeholder="输入QQ号" required="required" /><br>
     <button type="submit">删除！（注意此操作不可逆）</button>
 </form>
+/*需要验证用户名是否与添加者名字相同*/
