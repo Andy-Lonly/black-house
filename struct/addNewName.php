27dc9d24a44b84025818dcdbedc4ee5efa0c4ecc
Andy-Lonly <own@renme.org>
2014-10-16 17:42:21 +0800

    更新进去required属性

--- struct/addNewName.php
+++ struct/addNewName.php
@@ -1,7 +1,7 @@
 <form method="post" id="addNewRecForm" action="?mode=script&action=addNewRecord">
-	<input type="text" name="username" placeholder="用户名" /><br>
-    <input type="text" name="qqnum" placeholder="QQ号" /><br>
+	<input type="text" name="username" placeholder="用户名" required="required"/><br>
+    <input type="text" name="qqnum" placeholder="QQ号" required="required" /><br>
     <input type="number" name="level"max="3" min="0" step="1" required="required" placeholder="等级" /><br>
-    <textarea form="addNewRecForm" name="reason" placeholder="原因" /><br>
+    <textarea form="addNewRecForm" name="reason" placeholder="原因" required="required"/><br>
     <button type="submit">提交</button>
 </form>
