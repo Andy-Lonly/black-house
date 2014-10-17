160ffa10fd77ec2c2aec05e9ecea2441324e6ffa
Andy-Lonly <own@renme.org>
2014-10-17 18:26:08 +0800

    Create new file

---
+++ struct/select.php
@@ -0,0 +1,13 @@
+<form method="post" id="selectinfo" action="?mode=script&action=selectinfo"> 
+
+ 5
+<input type="text" name="seler" value="帐号" size="15" maxlength="200" /> 
+
+ 6
+<br /> 
+
+ 7
+<br /><input type="submit" name="up" value="提交" /> 
+
+ 8
+<input type="reset" name="r" value="重置" /> 
