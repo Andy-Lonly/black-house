3a3bddc4c85244b2f7353edd400be62d3ead1f97
Andy-Lonly <own@renme.org>
2014-10-17 18:50:10 +0800

    Update select.php

--- struct/select.php
+++ struct/select.php
@@ -1,13 +1,5 @@
-<form method="post" id="selectinfo" action="?mode=script&action=selectinfo"> 
-
- 5
-<input type="text" name="seler" value="帐号" size="15" maxlength="200" /> 
-
- 6
-<br /> 
-
- 7
-<br /><input type="submit" name="up" value="提交" /> 
-
- 8
-<input type="reset" name="r" value="重置" /> 
+<form method="post" id="selectinfo" action="?mode=script&action=selectinfo">
+<input type="text" name="seler" value="帐号" size="15" maxlength="200" />
+<br />
+<br /><input type="submit" name="up" value="提交" />
+<input type="reset" name="r" value="重置" />
