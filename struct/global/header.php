00e36b040a492f04792bf31d5aa84ac3f92b4d9b
Andy-Lonly <own@renme.org>
2014-10-17 19:10:36 +0800

    补充功能

--- struct/global/header.php
+++ struct/global/header.php
@@ -2,8 +2,10 @@
 	萌系AI联盟 x Niyume - 黑屋系统
 </div>
 <div id="headerSubContainer">
-	<a href="?mode="><li><i class="fa fa-home"></i> 黑屋主页</li></a>
-    <a href="?mode=editrecord"><li><i class="fa fa-plus-circle"></i> 添加/删除记录</li></a>
+    <a href="?mode="><li><i class="fa fa-home"></i> 黑屋主页</li></a>
+    <a href="?mode=add"><li><i class="fa fa-plus-circle"></i> 添加</li></a>
+    <a href="?mode=delect"><li><i class="fa fa-plus-circle"></i> 删除记录</li></a>
     <a href="?mode=select"><li><i class="fa fa-search"></i> 搜索记录</li></a>
+    <a href="?mode=view"><li><i class="fa fa-plus-circle"></i>浏览</li></a>
     <a href="?mode=about"><li><i class="fa fa-info"></i> 关于black-house</li></a>
 </div>
