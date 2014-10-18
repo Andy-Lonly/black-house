52490405b9d38ab23111fd0d5e484fb0865744ee
Andy-Lonly <own@renme.org>
2014-10-18 21:37:40 +0800

    CSS更新~

--- struct/addNewName.php
+++ struct/addNewName.php
@@ -1,7 +1,11 @@
+<link href="/style/form.css" rel="stylesheet" type="text/css">
+<meta charset="utf-8">
+<fieldset class="formxd">
 <form method="post" id="addNewRecForm" action="?mode=script&action=addNewRecord">
-	<input type="text" name="username" placeholder="用户名" required="required"/><br>
-    <input type="text" name="qqnum" placeholder="QQ号" required="required" /><br>
-    <input type="number" name="level"max="3" min="0" step="1" required="required" placeholder="等级" /><br>
-    <textarea form="addNewRecForm" name="reason" placeholder="原因" required="required"/><br>
-    <button type="submit">提交</button>
+	<input type="text" name="username" placeholder="用户名" required="required" class="forminput"/><br>
+    <input type="text" name="qqnum" placeholder="QQ号" required="required" class="forminput" /><br>
+    <input type="number" name="level"max="3" min="0" step="1" required="required" placeholder="等级"class="forminput" /><br>
+    <input type="text" name="reason" placeholder="原因" required="required" class="forminput" /><br>
+    <button type="submit" class="formbutton">提交</button>
 </form>
+</fieldset>
