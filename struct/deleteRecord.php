dd2f4a8bcd5cbb90d8daa5e982c5dd9f3d9d7717
Andy-Lonly <own@renme.org>
2014-10-18 21:37:02 +0800

    CSS更新~

--- struct/deleteRecord.php
+++ struct/deleteRecord.php
@@ -1,5 +1,10 @@
+<link href="/style/form.css" rel="stylesheet" type="text/css">
+<meta charset="utf-8">
+<fieldset class="formxd">
 <form method="post" action="?mode=script&action=deleteRecord">
-  <input type="text" name="qqnum" placeholder="输入QQ号" required="required" /><br>
-    <button type="submit">删除！（注意此操作不可逆）</button>
+  <input type="text" name="qqnum" placeholder="输入QQ号" required="required"  class="forminput" /><br>
+    <button type="submit" class="formbutton" >删除</button><br />
+    <p>注意此操作不可逆</P>
 </form>
+</fieldset>
 /*需要验证用户名是否与添加者名字相同*/
